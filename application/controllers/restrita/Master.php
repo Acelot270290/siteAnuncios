e62a9ab7924d023cb81198aff4523e63326c60f3
Acelot270290 <contato@adsites.org>
2021-09-09 13:30:48 -0300

    criação da categoria pai

--- application/controllers/restrita/Master.php
+++ application/controllers/restrita/Master.php
@@ -40,4 +40,108 @@ class Master extends CI_Controller {
 		$this->load->view('restrita/layout/footer');
 
 	}
+
+	public function core($categoria_pai_id = NULL){
+
+		$categoria_pai_id = (int) $categoria_pai_id;
+
+		if(!$categoria_pai_id){
+
+			/*
+			*Cadastrando a Categoria
+			*/
+
+		}else{
+
+			/*
+			*Verificamos se a categoria foi informa, ms devemos garantir sua existencia no banco de dados
+			*/
+
+			If(!$categoria = $this->core_model->get_by_id('categorias_pai', array('categoria_pai_id' => $categoria_pai_id))){
+
+				$this->session->set_flashdata('erro','Categoria não foi encontrada');
+				redirect('restrita/' . $this->router->fetch_class());
+
+			}else{
+				
+				/*
+				*Categoria encontrada e passamos para as validações
+				*/
+
+				$this->form_validation->set_rules('categoria_pai_nome', 'Categoria Pai' ,'trim|required|min_length[4]|max_length[40]|callback_valida_nome_categoria');
+				$this->form_validation->set_rules('categoria_pai_classe_icone', 'Icone da categoria' ,'trim|required|min_length[3]|max_length[20]');
+
+
+				if($this->form_validation->run()){
+
+					//Formulário validado... salvamos no banco de dados
+
+					echo '<prev>';
+					print_r($this->input->post());
+					echo "</pre>";
+					exit();
+
+				}else{
+					//erros de validação
+
+					$data = array(
+						'titulo'=>'Editar Categoria Pai',
+						'categoria' =>$categoria,
+					);
+			
+					/*echo '<prev>';
+					print_r($data['categoria']);
+					echo "</pre>";
+					exit();*/
+			
+					$this->load->view('restrita/layout/header',$data);
+					$this->load->view('restrita/master/core');
+					$this->load->view('restrita/layout/footer');
+				}
+
+			}
+
+
+		}
+
+
+
+	}
+
+	public function valida_nome_categoria($categoria_pai_nome){
+
+		$categoria_pai_id = $this->input->post('categoria_pai_id');
+
+		if(!$categoria_pai_id){
+
+			//Cadastrando
+
+			if($this->core_model->get_by_id('categoria_pai', array('categoria_pai_nome' => $categoria_pai_nome))){
+
+				$this->form_validation->set_message('valida_nome_categoria','Essa categoria já existe');
+
+				return false;
+
+			}else{
+
+				return true;
+
+			}
+
+			}else{
+
+				//Editando
+
+				if($this->core_model->get_by_id('categoria_pai', array('categoria_pai_nome' => $categoria_pai_nome, 'categoria_pai_id !='=> $categoria_pai_id))){
+
+					$this->form_validation->set_message('valida_nome_categoria','Essa categoria já existe');
+
+					return false;
+			}else{
+				return true;
+			}
+
+		}
+	}
+
 }
--- application/views/restrita/master/core.php
+++ application/views/restrita/master/core.php
@@ -4,297 +4,92 @@
 <!-- Main Content -->
 <div class="main-content">
 <section class="section">
-  <div class="section-body">
-    <div class="row">
-      <div class="col-12 col-md-12 col-lg-12">
-        <div class="card">
-		<form method="POST" name="form_core" accept-charset="utf-8" enctype="multipart/form-data">
-          <div class="card-header">
-            <h4> <?php echo $titulo; ?> </h4>
-          </div>
-          <div class="card-body">
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label>Nome</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-user text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control" name="first_name" value="<?php echo (isset($usuario) ? $usuario->first_name : set_value('first_name')); ?>">
+   <div class="section-body">
+      <div class="row">
+         <div class="col-12 col-md-12 col-lg-12">
+            <div class="card">
+               <form method="POST" name="form_core">
+                  <div class="card-header">
+                     <h4> <?php echo $titulo; ?> </h4>
                   </div>
-				  						<?php echo form_error('first_name', '<div class="text-danger">','</div>'); ?>
-                </div>
-				<div class="form-group col-md-6">
-                  <label>Sobrenome</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-user text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control" name="last_name" value="<?php echo (isset($usuario) ? $usuario->last_name : set_value('last_name')); ?>">
-                  </div>
-				  <?php echo form_error('last_name', '<div class="text-danger">','</div>'); ?>
-                </div>
-                
-              </div>
-			  <div class="form-row">
-                <div class="form-group col-md-4">
-                  <label>CPF</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-id-card text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control cpf" name="user_cpf" value="<?php echo (isset($usuario) ? $usuario->user_cpf : set_value('user_cpf')); ?>">
-                  </div>
-				  <?php echo form_error('user_cpf', '<div class="text-danger">','</div>'); ?>
-                </div>
-				<div class="form-group col-md-4">
-                  <label>Telefone</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-phone text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control sp_celphones" name="phone" value="<?php echo (isset($usuario) ? $usuario->phone : set_value('phone')); ?>">
-                  </div>
-				  <?php echo form_error('phone', '<div class="text-danger">','</div>'); ?>
-                </div>
-				<div class="form-group col-md-4">
-                  <label>Email</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-envelope text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control" name="email" value="<?php echo (isset($usuario) ? $usuario->email : set_value('email')); ?>">
-                  </div>
-				  <?php echo form_error('email', '<div class="text-danger">','</div>'); ?>
-                </div>
-                
-              </div>
-			  <div class="form-row">
-                <div class="form-group col-md-2">
-                  <label>CEP</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-map-marker-alt text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control cep" name="user_cep" value="<?php echo (isset($usuario) ? $usuario->user_cep : set_value('user_cep')); ?>">
-                  </div>
-				  <?php echo form_error('user_cep', '<div class="text-danger">','</div>'); ?>
-					<div id="user_cep"></div>
-                </div>
-				<div class="form-group col-md-3">
-                  <label>Endereço</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-road text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control" name="user_endereco" value="<?php echo (isset($usuario) ? $usuario->user_endereco : set_value('user_endereco')); ?>" readonly=''>
-                  </div>
-				  <?php echo form_error('user_endereco', '<div class="text-danger">','</div>'); ?>
-                </div>
-				<div class="form-group col-md-2">
-                  <label>Número</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-street-view text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control" name="user_numero_endereco" value="<?php echo (isset($usuario) ? $usuario->user_numero_endereco : set_value('user_numero_endereco')); ?>">
-                  </div>
-				  <?php echo form_error('user_numero_endereco', '<div class="text-danger">','</div>'); ?>
-                </div>
-				<div class="form-group col-md-2">
-                  <label>Bairro</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-directions text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control" name="user_bairro" value="<?php echo (isset($usuario) ? $usuario->user_bairro : set_value('user_bairro')); ?>" readonly=''>
-                  </div>
-				  <?php echo form_error('user_bairro', '<div class="text-danger">','</div>'); ?>
-                </div>
-				<div class="form-group col-md-2">
-                  <label>Cidade</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-location-arrow text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control" name="user_cidade" value="<?php echo (isset($usuario) ? $usuario->user_cidade : set_value('user_cidade')); ?>" readonly=''>
-                  </div>
-				  <?php echo form_error('user_cidade', '<div class="text-danger">','</div>'); ?>
-                </div>
-				<div class="form-group col-md-1">
-                  <label>Estado</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-map text-info"></i>
-                      </div>
-                    </div>
-                    <input type="text" class="form-control uf" name="user_estado" value="<?php echo (isset($usuario) ? $usuario->user_estado : set_value('user_estado')); ?>" readonly=''>
-                  </div>
-				  <?php echo form_error('user_estado', '<div class="text-danger">','</div>'); ?>
-                </div> 
-                
-              </div>
-			  <div class="form-row">  
-				<div class="form-group col-md-2">
-                  <label>Ativo</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-check-circle text-info"></i>
-                      </div>
-                    </div>
-                    <select class="custom-select" name="active">
+                  <div class="card-body">
 
-						<?php if(isset($usuario)){ ?>
+									<?php if(isset($categoria)){ ?>
 
-						<option value="0" <?php echo($usuario->active == 0 ? 'selected' : ''); ?>>Não</option>
-						<option value="1" <?php echo($usuario->active == 1 ? 'selected' : ''); ?>>sim</option>
-						<?php }else{ ?>
+										<div class="form-row">
+										<div class="form-group col-md-12">	
+										<label>Meta link da categoria</label>
+                           <div class="input-group">
+                              <div class="input-group-prepend">
+                                 <div class="input-group-text">
+                                    <i class="fas fa-link text-info"></i>
+                                 </div>
+                              </div>
+                              <input type="text" class="form-control"  value="<?php echo $categoria->categoria_pai_meta_link; ?>">
+                           </div>
+										</div>
+										</div>
 
-						<option value="0">Não</option>
-						<option value="1">sim</option>
-
-							<?php } ?>
-					</select>
+									<?php } ?>
+                     <div class="form-row">
+                        <div class="form-group col-md-4">
+                           <label>Nome da Categoria</label>
+                           <div class="input-group">
+                              <div class="input-group-prepend">
+                                 <div class="input-group-text">
+                                    <i class="fas fa-audio-description text-info"></i>
+                                 </div>
+                              </div>
+                              <input type="text" class="form-control" name="categoria_pai_nome" value="<?php echo (isset($categoria) ? $categoria->categoria_pai_nome : set_value('categoria_pai_nome')); ?>">
+                           </div>
+                           <?php echo form_error('categoria_pai_nome', '<div class="text-danger">','</div>'); ?>
+                        </div>
+                        <div class="form-group col-md-4">
+                           <label>Ícone da categoria</label>
+                           <div class="input-group">
+                              <div class="input-group-prepend">
+                                 <div class="input-group-text">
+                                    <i class="fas fa-cube text-info"></i>
+                                 </div>
+                              </div>
+                              <input type="text" class="form-control" name="categoria_pai_classe_icone" value="<?php echo (isset($categoria) ? $categoria->categoria_pai_classe_icone : set_value('categoria_pai_classe_icone')); ?>">
+                           </div>
+                           <?php echo form_error('categoria_pai_classe_icone', '<div class="text-danger">','</div>'); ?>
+                        </div>
+                        <div class="form-group col-md-4">
+                           <label>Ativa</label>
+                           <div class="input-group">
+                              <div class="input-group-prepend">
+                                 <div class="input-group-text">
+                                    <i class="fas fa-check-circle text-info"></i>
+                                 </div>
+                              </div>
+                              <select class="custom-select" name="categoria_pai_ativa">
+                                 <?php if(isset($categoria)){ ?>
+                                 <option value="0" <?php echo($categoria->categoria_pai_ativa == 0 ? 'selected' : ''); ?>>Não</option>
+                                 <option value="1" <?php echo($categoria->categoria_pai_ativa == 1 ? 'selected' : ''); ?>>sim</option>
+                                 <?php }else{ ?>
+                                 <option value="0">Não</option>
+                                 <option value="1">sim</option>
+                                 <?php } ?>
+                              </select>
+                           </div>
+                           <?php echo form_error('categoria_pai_ativa', '<div class="text-danger">','</div>'); ?>
+                        </div>
+                        <?php if(isset($categoria)){ //Validando o id do user ?>
+                        <input type="hidden" name="categoria_pai_id" value="<?php echo $categoria->categoria_pai_id; ?>">
+                        <?php } ?>
+                     </div>
                   </div>
-				  <?php echo form_error('active', '<div class="text-danger">','</div>'); ?>
-                </div>  
-				<div class="form-group col-md-3">
-                  <label>Perfil de Acesso</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-user-tie text-info"></i>
-                      </div>
-                    </div>
-                    <select class="custom-select" name="perfil">
-
-					<?php foreach($grupos as $grupo){ ?>
-
-						<?php if(isset($usuario)){ ?>
-
-							<option value="<?php echo $grupo->id; ?>" <?php echo ($perfil->id == $grupo->id ? 'selected' : ''); ?>><?php echo $grupo->description; ?></option>
-
-						<?php }else{ ?>
-
-						<option value="<?php echo $grupo->id; ?>"><?php echo $grupo->description; ?></option>
-
-							<?php } ?>
-					<?php } ?>
-					</select>
+                  <div class="card-footer">
+                     <button type="submit" class="btn btn-primary">Salvar</button>
+                     <a href="<?php echo base_url('restrita/' . $this->router->fetch_class()); ?>" class=" btn btn-dark">voltar</a>
                   </div>
-				  <?php echo form_error('perfil', '<div class="text-danger">','</div>'); ?>
-        </div>   
-
-			
-                <div class="form-group col-md-3">
-                  <label>Senha</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-lock text-info"></i>
-                      </div>
-                    </div>
-                    <input type="password" class="form-control" name="password" value="">
-                  </div>
-				  						<?php echo form_error('password', '<div class="text-danger">','</div>'); ?>
-                </div>
-
-								
-								<div class="form-group col-md-3">
-                  <label>Confirma Senha</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-lock text-info"></i>
-                      </div>
-                    </div>
-                    <input type="password" class="form-control" name="confirma_senha" value="">
-                  </div>
-				  						<?php echo form_error('confirma_senha', '<div class="text-danger">','</div>'); ?>
-                </div>
-
+               </form>
+            </div>
          </div>
-				 <div class="form-row"> 
-
-				 <div class="form-group col-md-4">
-                  <label>Avatar</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <div class="input-group-text">
-                        <i class="fas fa-image text-info"></i>
-                      </div>
-                    </div>
-                    <input type="file" class="form-control" name="user_foto_file">
-                  </div>
-				  <?php echo form_error('user_foto', '<div class="text-danger">','</div>'); ?>
-					<div id="user_foto"></div>
-        </div> 
-
-				<div class="form-group col-md-3">
-
-				<?php if(isset($usuario)){ ?>
-
-					<div id="box-foto-usuario">
-
-					<input type="hidden" name="user_foto" value="<?php echo $usuario->user_foto; ?>">
-					<img width="100" alt="Usuário imagem" src="<?php echo base_url('uploads/usuarios/' . $usuario->user_foto); ?>" class="rounded-circle">
-					</div>
-
-
-					<?php }else{ ?>
-
-						<div id="box-foto-usuario">
-
-						</div>
-
-
-						<?php } ?>
-
-						<?php if(isset($usuario)){ //Validando o id do user ?>
-
-							
-
-					<input type="hidden" name="usuario_id" value="<?php echo $usuario->id; ?>">
-
-
-							<?php } ?>
-                 
-        </div> 
-
-							
-				 </div>
-          </div>
-          <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Salvar</button>
-            <a href="<?php echo base_url('restrita/' . $this->router->fetch_class()); ?>" class=" btn btn-dark">voltar</a>
-          </div>
-		  </form>
-        </div>
+         <!-- Home da área restrita -->
       </div>
-      <!-- Home da área restrita -->
-    </div>
 </section>
 <div class="settingSidebar">
 <a href="javascript:void(0)" class="settingPanelToggle"> <i class="fa fa-spin fa-cog"></i>
